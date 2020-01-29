ee505e275c1caacc0418e6a94cc19dfbfe5fe074
Ruben <rmontill@student.42madrid.com>
2020-01-29 19:48:13 +0100

    Primeros cambios en SumaEnterosArray

--- SumaEnterosArray.java
+++ SumaEnterosArray.java
@@ -3,8 +3,35 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
-public class suma
+public class SumaEnterosArray
 {
+  public static void check (int[] num)
+  {
+    for (int i = 0; i < num.length; i++)
+      if (num[i] > 9 || num[i] < 0)
+      {
+        System.err.println("Error: el número proporcionado no existe");
+        System.exit(1);
+      }
+  }
+  public static int[] suma (int[] num_1, int[] num_2)
+  {
+    if (num_1.lenght <= num_2.length)
+      int[] result = new [num_2.length + 1];
+    if (num_1.lenght > num_2.length)
+      int[] result = new [num_1.length + 1];
+
+    int i = num_1.length - 1;
+    int j = num_2.length - 1;
+    int k = result.length - 1;
+    while (num_1[i] && num_2[j])
+    {
+      result[k] = num_1[i] + num_2[j];
+      i--;
+      j--;
+      k--; 
+    }
+  }
   public static void main (String[] args)
   {
     In n1 = new In(args[0]);
@@ -12,18 +39,8 @@ public class suma
     int[] num_1 = n1.readAllInts();
     int[] num_2 = n2.readAllInts();
 
-    for (int i = 0; i < num_1.length; i++)
-      if (num_1[i] > 9 && num_1[i] < 0)
-      {
-        System.err.println("Error: el número proporcionado no existe");
-        System.exit(1);
-      }
-    for (int i = 0; i < num_2.length; i++)
-      if (num_2[i] > 9 && num_2[i] < 0)
-      {
-        System.err.println("Error: el número proporcionado no existe");
-        System.exit(1);
-      }
+    check(num_1);
+    check(num_2);
     StdOut.println("Sumando; " + Arrays.toString(num_1) + Arrays.toString(num_2));
   }
 }
