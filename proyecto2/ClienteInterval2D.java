86db8756a695fba678a09f0905a12402c455c7d8
Ruben <rmontill@student.42madrid.com>
2020-02-07 10:07:22 +0100

    Primeras pruebas

--- proyecto2/ClienteInterval2D.java
+++ proyecto2/ClienteInterval2D.java
@@ -6,10 +6,18 @@ números reales introducidos como parámetros. Además hay que hayar si entre
 ellos son secantes o si hay alguno contenido en otro.
 */
 
+import java.util.Arrays;
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 public class ClienteInterval2D 
 {
-
+    public static void main(String[] args)
+    {
+        // Hay que probar este código para ver como lee los numeros y como los almacena. Dependiendo de la forma de estructurarlo habra que crear una funcion para que los separe en los rectangulos.
+        In in = new In(args[0]);
+        float[] rectangulos = in.readAllInts();
+        StdOut.println(Arrays.toString(rectangulos));
+    }
 }
