8f8f216b1d97c42a8ceb45944a0a3050f7a51a74
Ruben <rmontill@student.42madrid.com>
2020-02-10 19:50:08 +0100

    Varios cambios realizados en el ejercicio 3 y añadidos varios archivos de datos

--- proyecto2/LinearSearchCounter.java
+++ proyecto2/LinearSearchCounter.java
@@ -7,28 +7,34 @@ ya definidas previamente. Estas funciones son LinearSearch, SortedLinearSearch y
 */
 
 import java.util.Arrays;
+import org.w3c.dom.css.Counter;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 public class LinearSearchCounter
 {
-    public static int indexOf(int key,int[] a)
+    public static int indexOf(int key,int[] a, ft_counter count)
     {
         for(int i = 0; i < a.length; i++)
+        {
+            System.err.println("Traza: count = " + count.toString());
             if(a[i] == key) return i;
+            count.increment();
+        }
         return-1;
     }
     public static void main(String[] args)
     {
-        In           in =new In(args[0]);
-        int[] whitelist = in.readAllInts();
-        
+        In      in = new In(args[0]);
+        int[]   whitelist = in.readAllInts();
+        ft_counter count = new ft_counter();
+        System.err.println("Traza: count = " + count.toString());
         while(!StdIn.isEmpty())
         {
             int key = StdIn.readInt();
-            if(indexOf(key, whitelist) == -1)
-                StdOut.println(key);
-        }
+            if(indexOf(key, whitelist, count) == -1)
+                StdOut.println(key);        }
+        StdOut.println("Número de comparaciones: " + count.toString());
     }
 }
---
+++ proyecto2/ft_counter.java
@@ -0,0 +1,13 @@
+public class ft_counter
+{
+    private int count = 0;
+
+    public void increment()
+    { count++; }
+
+    public int tally()
+    { return count; }
+
+    public String toString()
+    { return "" + count; }
+}
