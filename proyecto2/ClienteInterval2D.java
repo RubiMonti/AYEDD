002fb3f1a0c2dc8a8219ad3301b1f6d7131160ee
Ruben <rmontill@student.42madrid.com>
2020-02-18 23:08:58 +0100

    Ejercicio 1 de proyecto2 casi acabado

--- proyecto2/ClienteInterval2D.java
+++ proyecto2/ClienteInterval2D.java
@@ -11,24 +11,71 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.Interval1D;
+import edu.princeton.cs.algs4.Interval2D;
+
 
 public class ClienteInterval2D 
 {
-    private double[] next_rect(double[] rectaungulos)
+    private static Double[] next_rect(String line)
     {
-        // creamos un punto con rectangulos[0-3]
-        double[] new_rectagulos = new double[rectaungulos.length - 4];
-        for (int i = 4; i < rectaungulos.length; i++)
-            new_rectagulos[i - 4] = rectaungulos[i];
-            return (new_rectagulos);
+        Double[] toreturn = new Double[4];
+        String[] points_s;
+        points_s = line.split(" ");
+        for (int i = 0; i < points_s.length ; i++)
+            System.err.println(points_s[i]);
+        if (points_s.length != 4)
+        {
+            System.err.println("El formato introducido no es válido, revisa los números");
+            System.exit(1);
+        }
+        Double number;
+        Double min = 0d;
+        Double max = 1d;
+        for (int i = 0; i < points_s.length; i++)
+        {
+            System.err.println(i);
+            try
+            {
+                number = Double.parseDouble(points_s[i]);
+                System.err.println(number);
+                if (Double.compare(number, min) >= 1 && Double.compare(number, max) <= 1)
+                    toreturn[i] = number;
+                else 
+                {
+                    System.err.println("El formato introducido no es válido, revisa los números");
+                    System.exit(1);
+                }
+            }
+            catch (Exception e)
+            {
+                System.err.println("El formato introducido no es válido, revisa los números");
+                System.exit(1);
+            }
+        }
+        return (toreturn);
     }
     public static void main(String[] args)
     {
         In in = new In(args[0]);
-        double[] rectangulos = in.readAllDoubles();
-        // Aqui creamos las variables que almacenaran los puntos de los retangulos.
-        StdOut.println(Arrays.toString(rectangulos));
-
-        next_rect(rectangulos);
+        Double[] rectangulos = new Double[4];
+        String line;
+        System.err.println("KLK");
+        int i = 0;
+        while (in.hasNextLine())
+        {
+            line = in.readLine();
+            rectangulos = new Double[4];
+            System.err.println(i + " " + line);
+            rectangulos = next_rect(line);
+            for (int j = 0; j < rectangulos.length ; j++)
+                System.err.println(rectangulos[j]);
+            Interval1D xInterval = new Interval1D(rectangulos[0], rectangulos[1]);
+            Interval1D yInterval = new Interval1D(rectangulos[2], rectangulos[3]);
+            Interval2D rect = new Interval2D(xInterval, yInterval);
+            rect.draw();
+            i++;
+        }
+        System.err.println(rectangulos.toString());
     }
 }
